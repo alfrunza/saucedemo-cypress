56283f96ddd12413171ab1ef1c40e33a63f6239f
alfrunza <94307925+alfrunza@users.noreply.github.com>
2025-11-10 12:38:35 +0200

    added yml file for github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'e6eqya',
+  projectId: "e6eqya",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // baseUrl: "https://www.saucedemo.com/",
     testIsolation: false,
   },
 });
